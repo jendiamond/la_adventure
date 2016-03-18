8ea0de393a018202965d5704c1a67b9a93b8aa6d
jendiamond <thejendiamond@gmail.com>
2016-03-18 04:16:31 +0000

    Change index to adventures#index, update Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LaAdventure::Application.routes.draw do
   resources :adventures
-
+  root 'adventures#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
