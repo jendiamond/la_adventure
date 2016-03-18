d3d4ccac02cf3f841fc4b8a4a459c28b8ff91a3b
jendiamond <thejendiamond@gmail.com>
2016-03-17 21:08:38 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LaAdventure::Application.config.secret_key_base = 'd252f1be2ddfeb6571f2d51e78ebe9032eb28ce99e80ddb479fe65ba8e3b65430a9f56d6d60ade64de50cf6abbb859f9cdaf60ef4671afbb3a7fdd41de838785'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LaAdventure::Application.config.session_store :cookie_store, key: '_la_adventure_session'
